11be601ae7f3b9d74bb9137b5ddfd52f8e4c4728
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-08-02 23:08:18 -0400

    Holi, aqui angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'crud';
+  productos = [{ nombre: 'prueba' }];
+
+  constructor(private api: ApiService) {
+    this.getProductos();
+  }
+
+  getProductos = () => {
+    this.api.getAllProductos().pipe(
+      tap((data) => {
+        this.productos = data;
+      }),
+      catchError((error) => {
+        console.log(error);
+        return of(null); 
+      })
+    ).subscribe();
+  }
 }
