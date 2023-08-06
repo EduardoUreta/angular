1c69f68e0a7d76f9abe35c72d3d08aac6cf3e33b
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-08-06 16:09:29 -0400

    img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { of } from 'rxjs';
   providers: [ApiService]
 })
 export class AppComponent {
-  productos = [{ nombre: '' },{precio: ''}];
+  productos = [{ nombre: '' },{precio: ''},{imagen: ''}];
 
   constructor(private api: ApiService) {
     this.getProductos();
